3fa2c3b6cb6ae24dacf1d1631c83d25229c522fd
Harrison Melton <hmmelton@comcast.net>
2016-11-06 20:28:19 -0500

    switched splash screen to launcher, finished splash screen

--- app/src/main/java/com/hmmelton/releave/Splashscreen.java
+++ app/src/main/java/com/hmmelton/releave/Splashscreen.java
@@ -1,7 +1,10 @@
 package com.hmmelton.releave;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Splashscreen extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class Splashscreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
+        // If the user is not logged in, navigate to login screen, else navigate to main page
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            startActivity(new Intent(this, LoginActivity.class));
+        } else {
+            startActivity(new Intent(this, MainActivity.class));
+        }
+        finish();
     }
 }
